abf3960333e125b31e466cb94a4edf3033468305
Suvanjan Prasai <73032557+suvanjanprasai@users.noreply.github.com>
2024-04-05 17:57:59 +0545

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template, request
+import finished.CustomCalender as CustomCalender
+import datetime
+
+app = Flask(__name__, template_folder="test",static_folder="css")
+
+
+
+@app.route("/", methods=["GET"])
+def home():
+    current_date = datetime.date.today()
+    current_year = current_date.year
+    months_data = {}
+    for month_num in range(1, 13):
+        month_name = datetime.date(current_year, month_num, 1).strftime("%B")
+        month_data = CustomCalender.get_calendar(current_year, month_num)
+        months_data[month_name.lower()] = month_data
+
+    for month, days in months_data.items():
+        for index, day_string in enumerate(days):
+            day_components = day_string.split()
+            for i, component in enumerate(day_components):
+                if component.isdigit():
+                    day_components[i] = f'<label for="{component.zfill(2)}{month[:3]}" class="custom-radio">{component.zfill(2)} </label>'
+                    day_components.append(f'<input id="{component.zfill(2)}{month[:3]}" type="radio" name="option" value="{component.zfill(2)}{month[:3]}">')
+            months_data[month][index] = '\n    '.join(day_components)
+
+    return render_template("index.html", months_data=months_data)
+
+@app.route("/submit", methods=["GET"])
+def submit():
+
+    return request.args
+app.run("127.0.0.1",port=4001)
